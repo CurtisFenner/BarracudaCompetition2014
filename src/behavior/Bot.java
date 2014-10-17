b34c204e1e1ad9d5813cca5cd2b0a2f3918e5596
Nathan Fenner <nfenneremail@gmail.com>
2014-10-17 19:32:01 -0400

    Behavior

--- src/behavior/Bot.java
+++ src/behavior/Bot.java
@@ -16,11 +16,39 @@ import com.barracuda.contest2014.PlayerWaitMessage;
 public class Bot {
 
 	public static PlayerMessage play(MoveRequestMessage input) {
+		// always at least one legal move in array
+		// choose best
+		// always going to choose bottom row for now
+		int bestChoice = 0;
+		int bestScore = 0;
+		int team = input.state.player;;
+		Board currentBoard = new Board(input.state.board);
+		for (int i = 0; i < input.state.legal_moves.length; i++) {
+			Point at = new Point(input.state.legal_moves[i]);
+			currentBoard.set(at, team);
+			int value = currentBoard.boardScore(team);
+			if (value > bestScore) {
+				bestChoice = i;
+				bestScore = value;
+			}
+		}
+
+		int[] choice = input.state.legal_moves[bestChoice];
+
+
+		if (Math.random() < 0.1) {
+			return new PlayerWaitMessage(input.id);
+		} else {
+			return new PlayerMoveMessage(input.id, choice);
+		}
+
+		/*
 		if (Math.random() < 0.5) {
 			return new PlayerWaitMessage(input.id);
 		} else {
 			int i = (int) (Math.random() * input.state.legal_moves.length);
 			return new PlayerMoveMessage(input.id, input.state.legal_moves[i]);
 		}
+		*/
 	}
 }
