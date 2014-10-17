ede4434983e41700fc53975127dbf6cfad611781
CurtisFenner <cfenneremail@gmail.com>
2014-10-17 19:27:27 -0400

    Point from Array Constructor

--- src/behavior/Point.java
+++ src/behavior/Point.java
@@ -12,6 +12,13 @@ public class Point {
 	public final int x;
 	public final int y;
 	public final int layer;
+	
+	public Point(int[] arr) {
+		this.x = arr[0];
+		this.y = arr[1];
+		this.layer = arr[2];
+	}
+	
 	public Point(int x,int y,int layer) {
 		this.x = x;
 		this.y = y;
