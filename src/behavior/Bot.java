5b917f74d4d86b201f5a3cfb882deb571089a7c1
Nathan Fenner <nfenneremail@gmail.com>
2014-10-18 00:37:13 -0400

    Board simulation

--- src/behavior/Bot.java
+++ src/behavior/Bot.java
@@ -15,60 +15,81 @@ import com.barracuda.contest2014.PlayerWaitMessage;
  */
 public class Bot {
 
-	public static Board simulatePlay(Board original, int team, int tokens, int count) {
-		Board board = original.copy();
-		for (int iterations = 0; iterations < count; iterations++) {
-			Point bestMove = null;
-			double baseScore = board.boardScore(team);
-			double bestDifference = 0;
-			//
-			Playability playability = new Playability(board, team);
-			for (int x = 0; x < 10; x++) {
-				for (int y = 0; y < 10 - x; y++) {
-					for (int layer = 0; layer < 10 - x - y; layer++) {
-						Point at = new Point(x, y, layer);
-						if (playability.get(at) && board.get(at) == 0) {
-							Board b = board.copy();
-							b.playAt(at, team);
-							double score = b.boardScore(team);
-							double efficiency = 1.0 / (1.0 + layer);
-							double difference = score - baseScore;
-							difference *= efficiency;
-							if (bestMove == null || difference > bestDifference) {
-								bestDifference = difference;
-								bestMove = at;
-							}
-						}
-					}
-				}
-			}
-			if (bestMove == null) { // end of game
-				return board;
-			}
-			if (bestMove.layer < tokens) {
-				// purchase it
-				tokens -= bestMove.layer + 1;
-				board.playAt(bestMove, team);
-			} else {
-				// wait once
-				tokens++;
-			}
-		}
-		return board;
-	}
+	public static int tokens_flag = 0;
 
-	public static PlayerMessage play(MoveRequestMessage input) {
-		int team = input.state.player;
-		Board board = new Board(input.state.board);
-		int tokens = input.state.tokens;
-		//
+	public static Board simulatePlay(Board original, int team, int tokens) {
+		Board board = original.copy();
 		Point bestMove = null;
 		double baseScore = board.boardScore(team);
 		double bestDifference = 0;
 		//
 		Playability playability = new Playability(board, team);
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10 - x; y++) {
+				for (int layer = 0; layer < 10 - x - y; layer++) {
+					Point at = new Point(x, y, layer);
+					if (playability.get(at) && board.get(at) == 0) {
+						Board b = board.copy();
+						b.playAt(at, team);
+						double score = b.boardScore(team);
+						double efficiency = 1.0 / (1.0 + layer);
+						double difference = score - baseScore;
+						difference *= efficiency;
+						if (bestMove == null || difference > bestDifference) {
+							bestDifference = difference;
+							bestMove = at;
+						}
+					}
+				}
+			}
+		}
+		if (bestMove == null) { // end of game
+			tokens_flag = tokens;
+			return board;
+		}
+		if (bestMove.layer < tokens) {
+			// purchase it
+			tokens -= bestMove.layer + 1;
+			board.playAt(bestMove, team);
+		} else {
+			// wait once
+			tokens++;
+		}
+		tokens_flag = tokens;
+		return board;
+	}
+
+	public static Board simulatePlay(Board original, int team, int tokens, int count) {
+		Board board = original.copy();
+		for (int iterations = 0; iterations < count; iterations++) {
+			board = simulatePlay(board, team, tokens);
+			tokens = tokens_flag;
+		}
+		tokens_flag = tokens;
+		return board;
+	}
+	public static final double FAILURE_REDUCTION = 0.15;
+
+	public static PlayerMessage play(MoveRequestMessage input) {
+		int team = input.state.player;
+		Board board = new Board(input.state.board);
+		int tokens = input.state.tokens;
+		int opponentTokens = input.state.opponent_tokens;
+		//
+		Point bestMove = null;
+		double baseScore = board.boardScore(team);
+		double bestDifference = 0;
+		//
+
+		Board boardWait = board.copy();
+		Playability[] waitPlayabilities = new Playability[10];
+
+		Playability playability = new Playability(board, team);
 		for (int layer = 0; layer < 10; layer++) {
-			
+			if (layer >= tokens) {
+				boardWait = simulatePlay(boardWait, team, tokens);
+			}
+			Playability waitPlayability = new Playability(boardWait, team);
 			for (int x = 0; x < 10 - layer; x++) {
 				for (int y = 0; y < 10 - x - layer; y++) {
 					Point at = new Point(x, y, layer);
@@ -79,7 +100,10 @@ public class Bot {
 						double efficiency = 1.0 / (1.0 + layer);
 						double difference = score - baseScore;
 						difference *= efficiency;
-						double failureReduction = 0.2;
+
+						if (!waitPlayability.get(at)) {
+							difference *= FAILURE_REDUCTION;
+						}
 
 						if (bestMove == null || difference > bestDifference) {
 							bestDifference = difference;
