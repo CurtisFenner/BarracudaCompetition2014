1068cd7c4303a70bb6abeab33f2ef0899725d3eb
Nathan Fenner <nfenneremail@gmail.com>
2014-10-17 22:13:01 -0400

    added minimax montecarlo

--- src/behavior/Bot.java
+++ src/behavior/Bot.java
@@ -4,11 +4,11 @@
  */
 package behavior;
 
-import com.barracuda.contest2014.ContestBot;
 import com.barracuda.contest2014.MoveRequestMessage;
 import com.barracuda.contest2014.PlayerMessage;
 import com.barracuda.contest2014.PlayerMoveMessage;
 import com.barracuda.contest2014.PlayerWaitMessage;
+import java.util.ArrayList;
 
 /**
  *
@@ -73,6 +73,54 @@ public class Bot {
 		}
 		return new Move(bestChoice, bestScore);
 	}
+
+	public static final int MOVES = 5;
+
+	public static Move chooseBest(Board board,int team,int depth) {
+		ArrayList<Point> valid = new ArrayList<Point>();
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10 - x; y++) {
+				Point at = new Point(x,y,0);
+				if (board.get(at) == 0) {
+					valid.add(at);
+				}
+			}
+		}
+		if (valid.isEmpty()) {
+			return new Move( null, board.boardScore(team) );
+		}
+		if (depth <= 0) {
+			// try five moves and choose move with best score
+			Move best = null;
+			for (int i = 0; i < MOVES; i++) {
+				Point choice = valid.get((int)(Math.random() * valid.size()));
+				board.set(choice, team);
+				double score = board.boardScore(team);
+				board.set(choice, 0);
+				if (best == null || best.score < score) {
+					best = new Move(choice,score);
+				}
+			}
+			return best;
+		}
+
+		Move best = null;
+		for (int i = 0; i < MOVES; i++) {
+			Point choice = valid.get((int)(Math.random() * valid.size()));
+			board.set(choice, team);
+			Move bestOpponentMove = chooseBest( board, board.opponentOf(team), depth-1 );
+			board.set(choice, 0);
+			if (best == null || best.score < -bestOpponentMove.score) {
+				best = new Move( choice, -bestOpponentMove.score );
+			}
+		}
+		return best;
+	}
+
+	public static Point chooseBest(Board board,int team) {
+		return chooseBest(board, team, 5).move;
+	}
+
 	public static int waiting = 0;
 	public static int playing = 0;
 
